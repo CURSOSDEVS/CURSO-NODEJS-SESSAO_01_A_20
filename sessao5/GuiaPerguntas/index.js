5f6374edf93b400f24c3127220e689e3300510e6
CLAUDISNEI <claudisneibello@gmail.com>
2021-08-01 17:22:32 -0300

    Instalando o bootstrap e limpando arquivos basicos, incluindo as referências na index.ejs para o booststrap funcionar na página do projeto

--- sessao5/GuiaPerguntas/index.js
+++ sessao5/GuiaPerguntas/index.js
@@ -9,46 +9,11 @@ app.set('view engine', 'ejs');
 //define que o aplicativo usará arquivos estaticos, informando 
 //em que pasta eles se estão localizados. Deve-se criar a pasta
 app.use(express.static('public'));
-
-//criando rota principal da aplicação e passando variaveis
-/*app.get('/', (req, res)=>{
-    //variavel que serão mostradas na pagina
-    var nome = 'Claudisnei';
-    var lang = 'Javascript';
-
-    res.render('index',{
-        nome: nome,
-        lang: lang,
-        empresa: 'Guia do programador',
-        inscritos: 8000
-    });
-});*/
-
-//estrutura de repeticão foreach
-var produtos = [
-    {nome:'Doritos', preco:3.14},
-    {nome:'Coca-Cola', preco:5},
-    {nome:'Leite', preco:1.45},
-    {nome:'Carne', preco:15},
-    {nome:'HeadBull', preco:6.45},
-
-]
-    
+ 
 //utilizando parâmetros e passando para a pagina
-app.get('/:nome/:lang', (req, res)=>{
-    //variavel que serão mostradas na pagina
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    var exibirmsg = false;
+app.get('/', (req, res)=>{
 
-    res.render('index',{
-        nome: nome,
-        lang: lang,
-        empresa: 'Guia do programador',
-        inscritos: 8000,
-        msg: exibirmsg,
-        produtos: produtos
-    });
+    res.render('index');
 });
 
 //rodando a aplicação
