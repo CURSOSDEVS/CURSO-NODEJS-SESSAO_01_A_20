eb083b378b5ee97f6114f232d520c1dd7edbbfcc
CLAUDISNEI <claudisneibello@gmail.com>
2021-08-01 13:42:38 -0300

    Exibir variáveis no HTML com o ejs

--- sessao5/GuiaPerguntas/index.js
+++ sessao5/GuiaPerguntas/index.js
@@ -6,9 +6,32 @@ const app = express();
 //estamos setando o ejs para trabalhar no express
 app.set('view engine', 'ejs');
 
-//criando rota principal da aplicação
-app.get('/', (req, res)=>{
-    res.render('principal/perfil');
+//criando rota principal da aplicação e passando variaveis
+/*app.get('/', (req, res)=>{
+    //variavel que serão mostradas na pagina
+    var nome = 'Claudisnei';
+    var lang = 'Javascript';
+
+    res.render('index',{
+        nome: nome,
+        lang: lang,
+        empresa: 'Guia do programador',
+        inscritos: 8000
+    });
+});*/
+
+//utilizando parâmetros e passando para a pagina
+app.get('/:nome/:lang', (req, res)=>{
+    //variavel que serão mostradas na pagina
+    var nome = req.params.nome;
+    var lang = req.params.lang;
+
+    res.render('index',{
+        nome: nome,
+        lang: lang,
+        empresa: 'Guia do programador',
+        inscritos: 8000
+    });
 });
 
 //rodando a aplicação
