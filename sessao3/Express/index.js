fd7219ad51bf5914293b01f48587430aa8031db7
CLAUDISNEI <claudisneibello@gmail.com>
2021-07-31 22:11:39 -0300

    Instalando o nodemo para reiniciar o servidor automaticamente, após alteracoes

--- sessao3/Express/index.js
+++ sessao3/Express/index.js
@@ -3,6 +3,24 @@ const express = require("express");
 //criando instancia do express
 const app = express();
 
+//chamando o aplicativo e criando uma rota inicial e 
+//definindo oque éla irá fazer
+app.get("/", function(req, res){
+    //enviando resposta, OBS: só podemos enviar a resposta
+    //uma vez se não a conexão irá se fechar.
+    res.send("Bem vindo ao guia do programador.");
+} );
+
+//criando uma rota para o blog
+app.get("/Blog", function(req, res){
+    res.send("<h1>Bem vindo ao meu Blog!.</h1>")
+});
+
+//criando uma rota para acessar o canal
+app.get("/canal/youtube", function(req, res){
+    res.send("<h1>Bem vindo ao meu canal!</h1>");
+});
+
 //iniciando o servidor
 app.listen(4000, function(erro)
 {
