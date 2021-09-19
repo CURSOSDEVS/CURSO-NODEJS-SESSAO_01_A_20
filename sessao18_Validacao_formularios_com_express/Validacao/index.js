c59177fc1cf279b1da47cd677e52e5f6cdf363bd
CLAUDISNEI <claudisneibello@gmail.com>
2021-09-19 12:09:32 -0300

    Criado a pasta views e dentro desta foi criado o arquivo index.ejs neste arquivo html configuramos um form básico para enviar para o arquivo index.js(backend) os valores dos campos do formulário.No arquivo index.js foi criado uma rota post para receber os valores e fazer uma validação, onde será retornado erro caso exista.

--- sessao18_Validacao_formularios_com_express/Validacao/index.js
+++ sessao18_Validacao_formularios_com_express/Validacao/index.js
@@ -27,9 +27,45 @@ app.use(flash());
 
 //teste para verificar se a aplicação está rodando
 app.get('/',(req,res)=>{
-    console.log("Aplicação rodando..");
-    res.send("Rodando...");
+   // console.log("Aplicação rodando..");
+    //res.send("Rodando...");
+    res.render("index");
 })
+
+//rota post que receberá os dados do formulário e fará a validação
+app.post('/form',(req,res)=>{
+    
+    //pega os dados passados no corpo da requisição e salva nas variáveis email, nome e ponto
+    var {email, nome, pontos} = req.body;
+
+    //cria as variaveis de erros que serão tratados
+    var emailError, nomeError, pontosError;
+
+    //validação do email caso ele não seja definido ou seja vazio
+    if(email == undefined || email == ""){
+        emailError = "O email não pode ser vazio";
+    }
+
+    //validação do campo pontos 
+    if(pontos == undefined || pontos < 20){
+        pontosError = "A pontuação não pode ser menor de 20";
+    }
+
+    //validação do campo nome
+    if(nome == undefined || nome == ""){
+        nomeError = "O nome não pode ser vazio";
+    }
+
+    //verifica se ocorreu qualquer erro de validação
+    if(emailError!=undefined || pontosError!=undefined || nomeError!=undefined){
+        res.send("Esse form está com erro");
+    }else{
+        res.send("Show de bola este Form");
+    }
+
+    
+
+});
 
 //rodando o servidor
 app.listen(4030, (req,res)=>{
