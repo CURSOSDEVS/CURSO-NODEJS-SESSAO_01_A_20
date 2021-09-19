440691d6e99082e956ff3cd6b87bdceb1a85e3db
CLAUDISNEI <claudisneibello@gmail.com>
2021-09-19 16:18:47 -0300

    Concluido a validação do formulário, a rota post após receber os valores dos campos realiza uma validação e gera erros, caso existam, esses erros são retornados juntamente com os valores passados são retornados para rota principal '/' atraves de sessions flash.Na rota principal então os valores e erros são renderizados para a página do form.

--- sessao18_Validacao_formularios_com_express/Validacao/index.js
+++ sessao18_Validacao_formularios_com_express/Validacao/index.js
@@ -2,6 +2,8 @@
 const express = require("express");
 const session = require("express-session");
 const flash = require("express-flash");
+const cookieParser = require("cookie-parser");
+
 
 
 //instancia do express
@@ -14,23 +16,48 @@ app.set('view engine','ejs');
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
+//ativando o cookie parser
+app.use(cookieParser("palavra secreta"));
+
 //configurando o express-session
 app.use(session({
     secret: "Keyboard Cat",
     resave: false,
     saveUninitialized:true,
-    cookie:{secure:true}
-}))
+    cookie:{ maxAge:60000 }
+})) 
 
 //configuração do express-flash
 app.use(flash());
 
-//teste para verificar se a aplicação está rodando
+//rota principal da aplicação
 app.get('/',(req,res)=>{
-   // console.log("Aplicação rodando..");
-    //res.send("Rodando...");
-    res.render("index");
-})
+   
+    //cria as variaveis de erros que receberão os valores das sessions-flash criadas na rota post
+    var emailError = req.flash("emailError");
+    var nomeError = req.flash("nomeError");
+    var pontosError = req.flash("pontosError");
+
+    //console.log(emailError);
+
+    //verifica se o erro existe, caso não exista será setado undefined na variável
+    emailError = (emailError == undefined || emailError == 0) ? undefined : emailError;
+    nomeError = (nomeError == undefined || nomeError == 0 )? undefined : nomeError;
+    pontosError = (pontosError == undefined || pontosError == 0)? undefined : pontosError;
+
+    //criando variaveis para receber o conteúdo dos campos e retornalos
+    //para o formulário
+    var email = req.flash("conteudoEmail");
+    var nome = req.flash('conteudoNome');
+    var pontos = req.flash('conteudoPontos');  
+
+    //verifica se existe valores passados senão seta
+    email = (email == undefined || email == 0)? undefined: email;
+    nome = (nome == undefined || nome == 0)? undefined: nome;
+    pontos = (pontos == undefined || pontos < 0)? undefined: pontos;
+
+    res.render("index",{emailError, pontosError, nomeError, email: email, nome: nome, pontos: pontos});
+});
 
 //rota post que receberá os dados do formulário e fará a validação
 app.post('/form',(req,res)=>{
@@ -39,11 +66,13 @@ app.post('/form',(req,res)=>{
     var {email, nome, pontos} = req.body;
 
     //cria as variaveis de erros que serão tratados
-    var emailError, nomeError, pontosError;
+    var emailError;
+    var nomeError;
+    var pontosError;
 
     //validação do email caso ele não seja definido ou seja vazio
     if(email == undefined || email == ""){
-        emailError = "O email não pode ser vazio";
+        emailError = "O email não pode ser vazio"; 
     }
 
     //validação do campo pontos 
@@ -56,14 +85,29 @@ app.post('/form',(req,res)=>{
         nomeError = "O nome não pode ser vazio";
     }
 
+    //verifica se o erro existe, caso não exista será setado undefined na variável
+    emailError = (emailError == undefined || emailError == 0) ? undefined : emailError;
+    nomeError = (nomeError == undefined || nomeError == 0 )? undefined : nomeError;
+    pontosError = (pontosError == undefined || pontosError == 0)? undefined : pontosError;
+
     //verifica se ocorreu qualquer erro de validação
     if(emailError!=undefined || pontosError!=undefined || nomeError!=undefined){
-        res.send("Esse form está com erro");
+        //console.log(emailError+pontosError+nomeError);
+        //criando sessions-flash para enviar o erro para outra rota
+        req.flash('emailError', emailError);
+        req.flash("pontosError", pontosError);
+        req.flash("nomeError",nomeError);
+
+        //criando flash-sessions para retornar o conteúdo dos campos para o formuláro 
+        //novamente e não deixar o campo em brando
+        req.flash('conteudoEmail', email);
+        req.flash('conteudoNome',nome);
+        req.flash('conteudoPontos',pontos);
+
+        res.redirect('/');
     }else{
         res.send("Show de bola este Form");
     }
-
-    
 
 });
 
