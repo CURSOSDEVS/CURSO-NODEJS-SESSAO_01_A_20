ed81ef8c0eb8d03e24edfb9ee2eb9442bc7db241
Claudisnei Alves <claudisneibello@gmail.com>
2021-12-21 08:49:29 -0300

    Trabalhando com framework VueJs

---
+++ sessao20_VueJs/guiaclientes/src/components/Cliente.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="cliente">
+        <h1 id="cliente-nome">Cliente aqui</h1>
+        <h3>Descrição do Cliente bla bla bla bla bla</h3>
+    </div>  
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    #cliente{
+        color: blue;
+        background-color: beige;
+        max-width: 600px;
+        height: 100px;
+    }
+    #cliente-nome{
+        color: green;
+    }
+</style>
---
+++ sessao20_VueJs/guiaclientes/src/components/Produto.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="produto">
+        <h3>Nome do  produto</h3>
+        <p>Bla bla bla bla bla bla</p>
+        <input id="preco" type="text" placeholder="preço">
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    #produto{
+        max-width: 600px;
+        background-color: brown;
+        color: azure;
+        
+    }
+    #preco{
+        background-color: antiquewhite;
+        
+    }
+</style>
